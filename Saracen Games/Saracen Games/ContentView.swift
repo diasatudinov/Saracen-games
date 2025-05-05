69e89afc6837e2182bd147ea901cf0a49c079b8c
Dias <dias.atudinov@gmail.com>
2025-05-05 17:08:16 +0500

    app setup

--- Saracen Games/Saracen Games/ContentView.swift
+++ Saracen Games/Saracen Games/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, мир!")
+                .font(.custom("Peace Sans Webfont", size: 17))
         }
         .padding()
     }
